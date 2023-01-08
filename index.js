d06805590abc1d9e149dfdc14d8184f0cb8c0fb6
shimiken <kenwshin@gmail.com>
2023-01-08 22:34:05 +0700

    Done Authentication

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
+const cookieSession = require("cookie-session");
 
 require("dotenv").config();
 
@@ -13,6 +14,8 @@ const PORT = 3000;
 const config = {
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -22,21 +25,41 @@ const AUTH_OPTIONS = {
 };
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log("accessToken", accessToken);
-  console.log("refreshToken", refreshToken);
-  console.log("profile", profile);
   done(null, profile);
 }
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((id, done) => {
+  // User.findById(id, (err, user) => {
+  //   done(null, user);
+  // });
+
+  done(null, id);
+});
 
 const app = express();
 
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
+
 app.use(passport.initialize());
+app.use(passport.session()); // -> deserializeUser is called
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  const isLoggedIn = req.isAuthenticated() && req.user;
 
   if (!isLoggedIn) {
     return res.status(401).json({
@@ -53,16 +76,20 @@ app.get(
   passport.authenticate("google", {
     successRedirect: "/",
     failureRedirect: "/failure",
-    session: false,
+    session: true,
   }),
   (req, res) => {
     console.log("Google called us back!");
   }
 );
 
-app.get("/auth/logout", (req, res) => {});
+app.get("/auth/logout", (req, res) => {
+  req.logout();
 
-app.get("/secure", checkLoggedIn, (req, res) => {
+  return res.redirect("/");
+});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Secure route");
 });
 
@@ -74,7 +101,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "/index.html"));
 });
 
-var http = require("http");
 https
   .createServer(
     {
